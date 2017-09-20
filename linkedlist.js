4a322cc8d13ed2df593e6809f608c8f5f8445cd7
Stephanie Barker <song_mage@yahoo.com>
2017-09-20 13:04:40 -0400

    linked list madness

---
+++ linkedlist.js
@@ -0,0 +1,16 @@
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.nextNode = null;
+	}
+}
+
+class LinkedList {
+	constructor(nodes) {
+		this.head = nodes[0];
+
+		for (let i = 0, len = nodes.length - 1; i < len; i++) {
+			//
+		}
+	}
+}
